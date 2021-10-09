1edc5b1e5a5fd81b9becd9e35cac660a8ee8586b
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-09 19:32:11 +0200

    final project

--- main.js
+++ main.js
@@ -1,29 +1,68 @@
-'use strict'
-let favouriteNumber = 4;
+'use strict';
+
+//PINTAR EN HTML UNA LISTA DE LOS PRIMEROS 150 POKEMON CON SUS FOTOS.
+
+const ulTotalPokemon = document.querySelector('.totalPokemonList');
+const totalPokemonButton = document.querySelector('.allButton');
+
+const getAllPokemons= async ()=>{
+    ulTotalPokemon.innerHTML = ''
+    const pokemonList = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=150")
+    const pokemonsToJson = await pokemonList.json();
+    const pokemonInfo = pokemonsToJson.results.map((pokemon,id)=>(
+        {
+        name:pokemon.name,
+        image:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id+1}.png`
+        }
+    ))
+    paintPokemon(pokemonInfo)
+}
+const paintPokemon = pokemons => {
+    const totalInfo = pokemons.map(
+        (pokemon) =>
+        `<li class="pokeList"><h1>${pokemon.name}</h1>
+        <img src="${pokemon.image}" alt="${pokemon.name}"/></li>`
+    ).join('');
+    ulTotalPokemon.innerHTML = totalInfo
+}
+
+totalPokemonButton.addEventListener('click', getAllPokemons)
+
+//BUSCADOR ÚNICO DE POKEMON
+
+const typeYourPokemon = document.querySelector('.typePokemon')
+const searchPokemonButton = document.querySelector('.searchPokemon')
+const OnePokemonInfo = document.querySelector('.onlyOne')
+
+const getOnlyOne = async () => {
+    const typeValue = typeYourPokemon.value
+    
+    const pokemonList = await fetch(`https://pokeapi.co/api/v2/pokemon/${typeValue}/`)
+    const pokemonsToJson = await pokemonList.json();
+    const pokemonInfo = 
+        {
+        name:pokemonsToJson.name,
+        image:`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${typeValue}.png`
+        }
+    OnePokemonInfo.innerHTML = `<li class="search"><h1>${pokemonInfo.name}</h1><img src="${pokemonInfo.image}"/></li>`
+}
+
+searchPokemonButton.addEventListener('click', getOnlyOne)
 
 
 
-const getRickAndMortyCharacter = async () => {
-    const result = await fetch(`https://rickandmortyapi.com/api/character/`);
-    const resultToJson = await result.json();
-    let allCharacters = resultToJson.results
-    console.log(allCharacters)
 
-    const ul = document.querySelector('.ul');
-    let ulContent = '';
-    allCharacters.map(character => {
-        const characterList = 
-        `<li><h1>${character.name}</h1> 
-        <img src="${character.image}" alt="${character.name}"/></li>`
-        ;
-        ulContent += characterList;
-    })
+const alertFunction = (e) =>{
+    e.target.removeEventListener(e.type, alertFunction)
+    swal('Recuerda que los pokemon no son infinitos...',)
+}
 
-    ul.innerHTML = ulContent;
-};
+const alertError = (e) => {
+    if (typeYourPokemon.value > 898){
+        e.target.removeEventListener(e.type, alertFunction)
+        swal('Recuerda que los pokemon no son infinitos...', 'Has introducido un número no válido', 'error')
+    } 
+}
 
+typeYourPokemon.addEventListener('change', alertError)
 
-
-
-
-getRickAndMortyCharacter()
