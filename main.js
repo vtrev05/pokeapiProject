075990a5707b3195b08df601fc2af59e560bf47b
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-10-12 17:27:49 +0200

    pokemon battle finished

--- main.js
+++ main.js
@@ -96,11 +96,12 @@ generatePokemonRandom.addEventListener('click', getRandomPokemon)
 
 const gen1 = document.querySelector('.generateRandom1')
 const gen2 = document.querySelector('.generateRandom2')
+
 const ulrandom1 = document.querySelector('.randomPoke1')
 const ulrandom2 = document.querySelector('.randomPoke2')
 
-let health1 = 50;
-let health2 = 50;
+let health1 = 10;
+let health2 = 10;
 
 const getRandomPokemon1 = async () => {
     const random = getRandomArbitrary(0, 898)
@@ -117,49 +118,51 @@ const getRandomPokemon1 = async () => {
     ulrandom1.innerHTML = `<li class="searcherli"><h1 class="onePokemonTitle firstRandom">${pokemonInfo.name}</h1><img src="${pokemonInfo.image}"/><h3>Habilidades</h3><ul><li class="searcher"><button class="ability11">${pokemonInfo.power[0]}</button></li><li class="searcher"><button class="ability12">${pokemonInfo.power[1]}</button></li><li class="searcher"><button class="ability13">${pokemonInfo.power[2]}</button></li></ul><h3>EL primer pokemon elegido es ${pokemonInfo.name} que cuenta con ${health1} ph de vida</h3><h4>Selecciona alguna de sus habilidades para atacar. Recuerda que ${pokemonInfo.name} ataca en primer lugar</h4>`
 
     //FIRST ROUND
-
+    let updatedHealth = health2-randomDamage
     const ab11 = document.querySelector('.ability11')
     const ab12 = document.querySelector('.ability12')
     const ab13 = document.querySelector('.ability13')
     const firstAttack = () => {
-        let updatedHealth = health2-randomDamage
-        if (randomDamage < 7) {
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. No es muy efectivo. La salud actual del pokemon rival es ${updatedHealth}</h3>`
-        } else if (randomDamage >= 10){
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es ${updatedHealth}</h3>`
-        } else {
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. La salud actual del pokemon rival es ${updatedHealth}</h3>`
-        }
         
-    }
-    const secondAttack = () => {
-        let updatedHealth = health2-randomDamage
         if (randomDamage < 7) {
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage} hp de daño. No es muy efectivo. La salud actual del pokemon rival es ${updatedHealth}</h3>`
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. No es muy efectivo. La salud actual del pokemon rival es <span class="finalHp1">${updatedHealth}</span></h3>`
         } else if (randomDamage >= 10){
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es ${updatedHealth}</h3>`
+            updatedHealth = 0;
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es <span class="finalHp1">${updatedHealth}</span></h3>`
         } else {
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage} hp de daño. La salud actual del pokemon rival es ${updatedHealth}</h3>`
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. La salud actual del pokemon rival es <span class="finalHp1"">${updatedHealth}</span></h3>`
+        }
+    } 
+    const secondAttack = () => {
+
+        if (randomDamage < 7) {
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage} hp de daño. No es muy efectivo. La salud actual del pokemon rival es <span class="finalHp1"">${updatedHealth}</span></h3>`
+        } else if (randomDamage >= 10){
+            updatedHealth = 0;
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es <span class="finalHp1"">${updatedHealth}</span></h3>`
+        }else {
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage} hp de daño. La salud actual del pokemon rival es <span class="finalHp1"">${updatedHealth}</span></h3>`
         }
     }
     const thirdAttack = () => {
-        let updatedHealth = health2-randomDamage
+
         if (randomDamage < 7) {
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage} hp de daño. No es muy efectivo. La salud actual del pokemon rival es ${updatedHealth}</h3>`
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage} hp de daño. No es muy efectivo. La salud actual del pokemon rival es <span class="finalHp1"">${updatedHealth}</span></h3>`
         } else if (randomDamage >= 10){
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es ${updatedHealth}</h3>`
-        } else {
-            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage} hp de daño. La salud actual del pokemon rival es ${updatedHealth}</h3>`
+            updatedHealth = 0;
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es <span class="finalHp1"">${updatedHealth}</span></h3>`
+        }else {
+            ulrandom1.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage} hp de daño. La salud actual del pokemon rival es <span class="finalHp1"">${updatedHealth}</span></h3>`
         }
         
     }
-    
+
     ab11.addEventListener('click', firstAttack)
     ab12.addEventListener('click', secondAttack)
     ab13.addEventListener('click', thirdAttack)
 }
 
-
+//GENERAR SEGUNDO POKEMON PARA LUCHAR
 
 const getRandomPokemon2 = async () => {
     const random = getRandomArbitrary(0, 898)
@@ -179,36 +182,39 @@ const getRandomPokemon2 = async () => {
     const ab21 = document.querySelector('.ability21')
     const ab22 = document.querySelector('.ability22')
     const ab23 = document.querySelector('.ability23')
-
+    let updatedHealth2 = health1-randomDamage2
     const firstAttack2 = () => {
-        let updatedHealth2 = health1-randomDamage2
+        
         if (randomDamage2 < 7) {
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. No es muy efectivo. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. No es muy efectivo. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
         } else if (randomDamage2 >= 10){
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            updatedHealth2 = 0;
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
         } else {
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
         }
         
     }
     const secondAttack2 = () => {
-        let updatedHealth2 = health1-randomDamage2
+
         if (randomDamage2 < 7) {
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage2} hp de daño. No es muy efectivo. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage2} hp de daño. No es muy efectivo. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
         } else if (randomDamage2 >= 10){
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage2} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
-        } else {
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage2} hp de daño. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            updatedHealth2 = 0;
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
+        }else {
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[1]} que hace ${randomDamage2} hp de daño. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
         }
     }
     const thirdAttack2 = () => {
-        let updatedHealth2 = health1-randomDamage2
+
         if (randomDamage2 < 7) {
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage2} hp de daño. No es muy efectivo. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage2} hp de daño. No es muy efectivo. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
         } else if (randomDamage2 >= 10){
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage2} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            updatedHealth2 = 0;
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[0]} que hace ${randomDamage2} hp de daño. Has dado un golpe crítico!. La salud actual del pokemon rival es <span class="finalHp2"">${updatedHealth2}</span></h3>`
         } else {
-            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage2} hp de daño. La salud actual del pokemon rival es ${updatedHealth2}</h3>`
+            ulrandom2.innerHTML += `<h3>Has seleccionado la habilidad ${pokemonInfo.power[2]} que hace ${randomDamage2} hp de daño. La salud actual del pokemon rival es <span class="finalHp2">${updatedHealth2}</span></h3>`
         }
         
     }
@@ -223,10 +229,29 @@ const getRandomPokemon2 = async () => {
 gen1.addEventListener('click', getRandomPokemon1)
 gen2.addEventListener('click', getRandomPokemon2)
 
+//MOSTRAR RESULTADO FINAL
+
+const container = document.querySelector('.pokemonfight')
+const finalBtn = document.querySelector('.result')
+
+const printResult = () => {
+    const spanValue = document.querySelector('.finalHp1')
+    const spanValue2 = document.querySelector('.finalHp2')
+    let numberResult1 = parseInt(spanValue.textContent)
+    let numberResult2 = parseInt(spanValue2.textContent)
 
 
+    if (numberResult1 > numberResult2) {
+        container.innerHTML += `<h3>Ha ganado el Pokemon 2, que se ha quedado a ${numberResult1} HP de vida frente a los ${numberResult2} HP del Pokemon 1. ENHORABUENA AL GANADOR!</h3>`
+        
+    } else if (numberResult1 === numberResult2) {
+        container.innerHTML += `<h3>Ha El resultado ha sido EMPATE. Ambos pokemon se han quedado con ${numberResult1} HP de vida. GRACIAS POR PARTICIPAR!</h3>`
+    } else {
+        container.innerHTML += `<h3>Ha ganado el Pokemon 1, que se ha quedado a ${numberResult2} HP de vida frente a los ${numberResult1} HP del Pokemon 2. ENHORABUENA AL GANADOR!</h3>`
+    }
+}
 
 
-
+finalBtn.addEventListener('click', printResult)
 
 
